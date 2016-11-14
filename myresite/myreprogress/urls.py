1be794f14142decef3c3cb920c4344ebb8ec3fd6
Highstaker <heights999@yandex.ru>
2016-11-14 11:13:59 +0300

    v0.1.3
    +login and logout

--- myresite/myreprogress/urls.py
+++ myresite/myreprogress/urls.py
@@ -1,10 +1,13 @@
 from django.conf.urls import url
 
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^index\.html$', views.index, name='index'),
     url(r'^book/$', views.book_choice, name='Book selection'),
     url(r'^book/(?P<book_id>[0-9]+)/$', views.book_stats, name='Book stats'),
+    url(r'^login/$', auth_views.login, name='login'),
+    url(r'^logout/$', auth_views.logout, {'next_page': "/"}, name='logout'),
 ]
--- myresite/myreprogress/views.py
+++ myresite/myreprogress/views.py
@@ -21,3 +21,4 @@ def book_stats(request, book_id):
     book_name = get_object_or_404(Book, pk=book_id)
     book_pages = BookPage.objects.filter(book=book_id)
     return render(request, "myreprogress/book_stats.html", {"book_name": book_name, "book_pages": book_pages})
+
