2678fede048b62943f1e7f6b387a47492beb43f2
Highstaker <heights999@yandex.ru>
2016-12-26 22:42:15 +0300

    v0.4.2
    +batch deletion of pages in admin should now perform validation afterwards

--- myresite/myreprogress/admin.py
+++ myresite/myreprogress/admin.py
@@ -6,9 +6,10 @@ from .models import BookPage, Book
 
 class BookPageAdmin(admin.ModelAdmin):
 	# fields NOT to show in Edit Page.
-	list_display = ('__str__', 'page_name', 'sketched','colored','edited','proofread',)
+	list_display = ('__str__', 'page_name', 'sketched', 'colored', 'edited', 'proofread',)
 	list_filter = ('book',)
 	readonly_fields = ('page_number',)  # valid page number is assigned via overridden save() in model
+	actions = ['delete_selected',]
 
 	def save_model(self, request, obj, form, change):
 		if not change:
@@ -18,16 +19,21 @@ class BookPageAdmin(admin.ModelAdmin):
 		obj.save()  # the parent does only this
 
 	def delete_model(self, request, obj):
-		# page_num = obj.page_number
 		book = obj.book
 		super(BookPageAdmin, self).delete_model(request, obj)
 		book.validatePageNumbers()
 
+	def delete_selected(self, request, obj):
+		# kinda overriding default 'delete_selected' action to make it perform page validation afterwards
+		books = {i.book for i in obj}
+		obj.delete()
+		for b in books:
+			# perform validation for all books the pages of which were deleted
+			b.validatePageNumbers()
+
 
 class BookAdmin(admin.ModelAdmin):
-	prepopulated_fields = {'book_slug': ('book_name',)}
 	list_display = ('book_name', 'book_slug',)
-	pass
 
 admin.site.register(BookPage, BookPageAdmin)
 admin.site.register(Book, BookAdmin)
