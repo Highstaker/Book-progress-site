42d29ae409531aa34ada91c475d0ccbe9531005d
Highstaker <heights999@yandex.ru>
2017-04-20 07:36:01 +0300

    huge changes in styling Implemented bootstrap

--- myresite/myreprogress/views.py
+++ myresite/myreprogress/views.py
@@ -21,13 +21,8 @@ class BookChoiceView(ListView):
     def get_context_data(self, **kwargs):
         # Call the base implementation first to get a context
         context = super(BookChoiceView, self).get_context_data(**kwargs)
-        context['important_data'] = 'temmie Sez: "h0i!!11"'
+        # context['important_data'] = 'temmie Sez: "h0i!!11"'
         return context
-
-# old function-based implementation for BookChoiceView
-# def book_choice(request):
-#     books = Book.objects.all()
-#     return render(request, "myreprogress/book_choice.html", {"books": books})
 
 
 class BookStatsView(ListView):
@@ -55,29 +50,3 @@ class BookStatsView(ListView):
         context['insert_page_form'] = insert_page_form
 
         return context
-
-# old function-based implementation for BookStatsView
-# def book_stats(request, book_id=None, book_slug=None):
-#     if book_slug:
-#         book = get_object_or_404(Book, book_slug=book_slug)
-#         book_id = book.pk
-#     book_name = get_object_or_404(Book, pk=book_id)
-#     book_pages = BookPage.objects.getSortedPagesQueryset(book_id)
-#
-#     # insert page form
-#     insert_form = None
-#     if request.user.is_staff:
-#         if request.method == 'POST':
-#             insert_form = PageInsertForm(request.POST)
-#
-#             if insert_form.is_valid():
-#                 pass
-#                 # nothing happens here, everything is handled via JS and AJAX
-#                 # print("LOL"*100)
-#         else:
-#             insert_form = PageInsertForm()
-#     else:
-#         insert_form = None
-#
-#     return render(request, "myreprogress/book_stats.html", {"book_id": book_id, "book_name": book_name,
-#                                                             "book_pages": book_pages, "insert_page_form": insert_form})
