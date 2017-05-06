7470467fec7314011004e09472cdea2e35571145
Highstaker <heights999@yandex.ru>
2017-05-06 09:24:10 +0300

    +added registration

--- myresite/myresite/urls.py
+++ myresite/myresite/urls.py
@@ -19,5 +19,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^', include('myreprogress.urls')),
     url(r'^admin/', admin.site.urls),
-    # url(r'^progress/', include('myreprogress.urls')),
+    url(r'^accounts/', include('registration.backends.simple.urls')),
 ]
