6d145e2b0b92f03a231f625b49a448bdd12dbca3
Highstaker <heights999@yandex.ru>
2017-05-06 08:20:43 +0300

    +took common settings from test and production into a separate file

--- myresite/myresite/prod_settings.py
+++ myresite/myresite/prod_settings.py
@@ -12,15 +12,9 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 Run with DJANGO_SETTINGS_MODULE=myresite.prod_settings
 """
 
+from myresite.common_settings import *
 import os
 from django.utils.crypto import get_random_string
-
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 try:
@@ -43,94 +37,6 @@ ALLOWED_HOSTS = [
                  ]
 
 
-# Application definition
-
-INSTALLED_APPS = [
-    'myreprogress.apps.MyreprogressConfig',
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'django_extensions',
-]
-
-MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-]
-
-ROOT_URLCONF = 'myresite.urls'
-
-TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
-        },
-    },
-]
-
-WSGI_APPLICATION = 'myresite.wsgi.application'
-
-
-# Database
-# https://docs.djangoproject.com/en/1.10/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
-
-# Password validation
-# https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
-
-AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
-]
-
-
-# Internationalization
-# https://docs.djangoproject.com/en/1.10/topics/i18n/
-
-LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'UTC'
-
-USE_I18N = True
-
-USE_L10N = True
-
-USE_TZ = True
-
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 #MAKE SURE that www-data has execution priviliges on all folders leading to static folder, or 403 will be raised.
@@ -138,14 +44,6 @@ USE_TZ = True
 from myresite import static_root
 STATIC_ROOT = static_root.STATIC_ROOT
 print("STATIC_ROOT", STATIC_ROOT)#debug
-
-STATIC_URL = '/static/'
-
-STATICFILES_DIRS = (
-os.path.join(BASE_DIR, 'static/'),
-)
-
-LOGIN_REDIRECT_URL = "/book/"
 
 # SECURITY SETTINGS
 
